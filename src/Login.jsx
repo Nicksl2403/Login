6304848d8983291f73ab11363d53f17658430ba0
Nicksl2403 <nicolasfaustino2012@gmail.com>
2026-08-06 15:29:18 -0300

    V2.0

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,17 +1,44 @@
   import { useState, useEffect, useRef } from 'react'
-  import { BrowserRouter, Link, Route, Routes, useLocation, Navigate, useNavigate, useResolvedPath } from 'react-router-dom';
+  import { BrowserRouter, Link, Route, Routes, useLocation, Navigate, useNavigate, useResolvedPath, useAsyncError } from 'react-router-dom';
   import './CSS/indexLogin.css'
   import Calculadora from './Jogos/calculadora.jsx';
   import Botao from './Jogos/botao.jsx';
   import Velha from './Jogos/velha.jsx';
   import Painel from "./Painel.jsx";
+  import Logo from "./Jogos/imagens/logoEmpresa.png"
+  import senhas from "./.gitignore/senhas.jsx"
   function Login() {
     const [logado, setLogado] = useState(false);
-    const [passouMouse, setPassouMouse] = useState(false);
+    const [passouMouseBotao, setPassouMouseBotao] = useState(false);
     const [nomeInput, setNomeInput] = useState("");
+    const [senhaInput, setSenhaInput] = useState("");
+    const [passouMouseNome, setPassouMouseNome] = useState("");
+    const[passouMouseSenha, setPassouMouseSenha] = useState("");
+    const [tecla, setTecla] = useState("");
     const navigate = useNavigate();
     const location = useLocation();
-    const sites = ["/Login","/Calculadora","/Botao","/Jogo-Da-Velha","/Painel"];
+     useEffect(() => {
+    function teclaApertada(evento) {
+      if(evento.key == "Enter") {
+         if(String(nomeInput) in senhas) {
+            if((String(nomeInput)) === "ADM") {
+              setLogado(true); navigate("/Painel");
+            } else if(Number(senhaInput) === senhas[nomeInput]) {
+               setLogado(true); navigate("/Painel");
+              } else {
+                let alerta = alert("Senha incorreta");
+              }
+            } else {
+               let alerta = alert("Usuario nao encontrado");
+            }
+      }
+  
+  }
+  window.addEventListener("keydown", teclaApertada);
+    return () => {
+    window.removeEventListener("keydown", teclaApertada);
+  };
+}, [nomeInput,senhaInput]);
     return (
         <>
         {location.pathname === "/Login" && !logado && ( 
@@ -28,7 +55,7 @@
       color: "white",
       display:"flex",
       flexDirection: "column",
-      backgroundColor: "gray",
+      backgroundColor: "black",
       alignItems:"center",
       borderRadius:"20px",
       boxShadow: 
@@ -36,30 +63,52 @@
       0 0 20px #00ff88,
       inset 0 0 40px rgba(23,250,144,0.6)
       `,
-      gap:"10px",
+      gap:"20px",
         }}> 
-      
-        <input type="text" placeholder="Digite o seu usuario" value={nomeInput} onChange={(evento) => setNomeInput(evento.target.value)}style={{
+        <div style={{
+          marginTop:"15px",
+        }}></div>
+        <img src={Logo} width={200} height={80} />
+        <input type="text" placeholder="Digite o seu usuario" value={nomeInput} onChange={(evento) => setNomeInput(evento.target.value)} onMouseEnter={() => {setPassouMouseNome(true)}}onMouseLeave={() => {setPassouMouseNome(false)}}style={{
           background:"black",
           color:"white",
           textAlign:"center",
-          border:"0px solid black",
-          marginTop:"150px",
+          border: passouMouseNome ? "2px solid seagreen" : "2px solid black",
+          marginTop:"0px",
           height:"30px",
-          width:"220px"
+          width:"220px",
+          outline: "none"
         }}></input>
-        <button onMouseEnter={() => setPassouMouse(true)}onMouseLeave={() => setPassouMouse(false)} onClick={() => {
-          if(nomeInput === "Nicolas") {
-             setLogado(true);navigate("/Painel")
-            } else { 
-              alerta = alert("Usuario Invalido")
-            }}
-          } style={{
+          <input type="password" placeholder="Digite sua senha" value={senhaInput} onChange={(evento) => {setSenhaInput(evento.target.value)}} onMouseEnter={() => {setPassouMouseSenha(true)}} onMouseLeave={() => {setPassouMouseSenha(false)}} style={{
+          background:"black",
+          color:"white",
+          textAlign:"center",
+          border: passouMouseSenha ? "2px solid seagreen" : "2px solid black",
+          marginTop:"0px",
+          height:"30px",
+          width:"220px",
+          outline: "none"
+        }}></input>
+        <button onMouseEnter={() => setPassouMouseBotao(true)}onMouseLeave={() => setPassouMouseBotao(false)} onClick={() => {
+          if(String(nomeInput) in senhas) {
+            if((String(nomeInput)) === "ADM") {
+              setLogado(true); navigate("/Painel");
+            } else if(Number(senhaInput) === senhas[nomeInput]) {
+               setLogado(true); navigate("/Painel");
+              } else {
+                let alerta = alert("Senha incorreta");
+              }
+            } else {
+               let alerta = alert("Usuario nao encontrado");
+            }
+          }} style={{
           width:"150px",
           height:"50px",
           background: "transparent",
           fontSize:"23px",
-          border: passouMouse ? "2px solid black" : "2px solid transparent",
+          background:"transparent",
+          border: passouMouseBotao ? "2px solid seagreen" : "2px solid transparent",
+          color:"white",
         }}><b>Entrar</b></button>
         </div>
         </div>
@@ -67,9 +116,9 @@
         </>
         )}
       <Routes>
-      <Route path="/Calculadora" element={<Calculadora />} />
-      <Route path="/Botao" element={<Botao />} />
-      <Route path="/Jogo-Da-Velha" element={<Velha />} />
+      <Route path="/Calculadora" element={logado ? <Calculadora /> : <Navigate to="/Login"/>} />
+      <Route path="/Botao" element={logado ? <Botao /> : <Navigate to="/Login"/>} />
+      <Route path="/Jogo-Da-Velha" element={logado ? <Velha /> : <Navigate to="/Login"/> } />
       <Route path="/Painel" element={logado ? <Painel /> : <Navigate to="/Login"/>} /> 
       <Route path="/Login" element={<Login />} />
       <Route path="*" element={<Navigate to="/Login" />} />
---
+++ src/senhas.jsx
@@ -0,0 +1,9 @@
+const senhas = {
+    "Nicolas": 101325,
+    "Giovanni": 101325,
+    "Miguel": 101325,
+    "Felipe": 101325,
+    "Rodrigo": 101325,
+    "Daniele": 101325,
+}
+export default senhas;
